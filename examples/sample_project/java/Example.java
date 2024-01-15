136049c0503536878d1bde7090be3e8aa9d1c6be
Scott Minor <minor@minorhacks.com>
2024-01-15 01:58:15 -0500

    Examples for Go and Java added

---
+++ examples/sample_project/java/Example.java
@@ -0,0 +1,9 @@
+import build.bazel.remote.asset.v1.RemoteAssetProto;
+import build.bazel.remote.execution.v2.RemoteExecutionProto;
+import build.bazel.remote.logstream.v1.RemoteLogStreamProto;
+
+class Example {
+  public static void main(String []args) {
+    System.out.println("Java example compiles and runs!");
+  }
+}
